0900d0250e1516b26b3b303418f727d87a5bfa21
Isaac Perkins <iperkins@uoregon.edu>
2024-02-27 14:30:22 -0800

    Added loading circle

--- app/src/main/java/com/example/notduo/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/notduo/navigation/AppNavigation.kt
@@ -1,12 +1,17 @@
 package com.example.notduo.navigation
 
 import android.content.Context
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -20,19 +25,27 @@ import com.example.notduo.screens.RegistrationScreen
 fun AppNavigation(context: Context) {
     val navController = rememberNavController()
     var sessionIsActive by remember { mutableStateOf(false) }
+    var isLoading by remember { mutableStateOf(true) }
 
     // Check if we are logged in
     LaunchedEffect(Unit) {
         sessionIsActive = SessionManager.isSessionActive(context)
+        isLoading = false
     }
 
-    val startDestination = if (sessionIsActive) "mainApp" else "registration"
-
-    NavHost(navController = navController, startDestination = startDestination) {
-        composable("registration") {
-            val registrationViewModel = viewModel<RegistrationViewModel>()
-            RegistrationScreen(navController, registrationViewModel, context)
+    if (isLoading) {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
         }
-        composable("mainApp") { MainAppScreen() }
+    } else {
+        val startDestination = if (sessionIsActive) "mainApp" else "registration"
+
+        NavHost(navController = navController, startDestination = startDestination) {
+            composable("registration") {
+                val registrationViewModel = viewModel<RegistrationViewModel>()
+                RegistrationScreen(navController, registrationViewModel, context)
+            }
+            composable("mainApp") { MainAppScreen() }
+        }
     }
 }
